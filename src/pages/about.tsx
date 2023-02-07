e45e41b8fc7dd9ebb23f4999340fe8ae519eaf5e
vsheva <valera.sheva@gmail.com>
2023-02-06 21:15:54 -0500

    fix

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,55 +1,49 @@
 import Link from 'next/link';
 import React, { useState, useEffect } from 'react';
 
-const About = (props: any) => {
-  const [ID, setId] = useState('1');
-  const [post, setPost] = useState([]);
+const About = (props:any) => {
+  const[ID, setID] = useState("1");
+  const[post, setPost] = useState("");
 
-  //console.log("message", props.message)
-
-  // строку в массив с помощью parse
-  //в 33 строчке - массив в строку
-  useEffect(() => {
-    setPost(JSON.parse(props.message).filter((post: any) => post.id === ID));
-  }, [props.message, ID]);
+  useEffect(() =>{
+    setPost(props.message.filter((post:any)=> post.id===ID))
+  }, [props.message, ID])
 
   return (
     <>
-      <h1>About</h1>
-      <Link href="/">{`Hello:`}</Link>
+      <div>ABOUT PAGE</div>
+      <Link href="/">Back</Link>
       <div>
-        <button onClick={() => setId('1')}>1</button>
+        <button onClick={()=>setID("1")}>1</button>
       </div>
       <div>
-        <button onClick={() => setId('2')}>2</button>
+        <button onClick={()=>setID("2")}>2</button>
       </div>
       <div>
-        <button onClick={() => setId('3')}>3</button>
+        <button onClick={()=>setID("3")}>3</button>
       </div>
       <div>
-        <button onClick={() => setId('4')}>4</button>
+        <button onClick={()=>setID("4")}>4</button>
       </div>
-      {ID !== '' && <div>{JSON.stringify(post)}</div>}
+      <div>
+        <button onClick={()=>setID("5")}>5</button>
+      </div>
+      {ID && <div>{JSON.stringify(post)}</div>}
     </>
   );
 };
 
 export default About;
 
-//SSR
-
 export async function getServerSideProps() {
-  let response = await fetch('https://62e7897793938a545bd3a4cc.mockapi.io/api/v1/tasks');
+  const response = await fetch('https://62e7897793938a545bd3a4cc.mockapi.io/api/v1/tasks');
+  const data = await response.json();
 
-  let data = await response.json(); //array
-
-  //делаем из массива строку
   return {
-    props: { message: JSON.stringify(data) },
+    props: { message: data },
   };
 }
 
-const name = '1';
 
 /*
  await fetch('https://62e7897793938a545bd3a4cc.mockapi.io/api/v1/tasks')
