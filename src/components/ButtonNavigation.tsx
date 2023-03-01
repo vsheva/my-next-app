8ecd451bc8f0b020f5c53be54d981e72bf4e7a6b
vsheva <valera.sheva@gmail.com>
2023-02-28 21:24:33 -0500

    Styled components

---
+++ src/components/ButtonNavigation.tsx
@@ -0,0 +1,13 @@
+import styled from '@emotion/styled';
+import { Button } from '@mui/material';
+
+const ButtonNavigation = styled(Button)<any>`
+  color: black;
+  /* &:hover {
+    background-color: black;
+    opacity: 5%;
+    z-index: -1;
+  } */
+`;
+
+export default ButtonNavigation;
---
+++ src/components/LinkNavigation.tsx
@@ -0,0 +1,9 @@
+import styled from '@emotion/styled';
+import Link from 'next/link';
+
+const LinkNavigation = styled(Link)<any>`
+  text-decoration: none;
+  color: black;
+`;
+
+export default LinkNavigation;
